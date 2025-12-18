f498e2ba7086892aefd3de395e2e37105eb2ce5c
Code Flow Dev <dev@codeflow.local>
2025-12-18 12:16:26 -0300

    Improve serverless handler with error response

--- api/[...all].ts
+++ api/[...all].ts
@@ -5,9 +5,15 @@ import { buildApp } from "../server/app";
 let handler: ((req: VercelRequest, res: VercelResponse) => Promise<void>) | null = null;
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  if (!handler) {
-    const app = await buildApp();
-    handler = serverlessHttp(app) as any;
+  try {
+    if (!handler) {
+      const app = await buildApp();
+      handler = serverlessHttp(app) as any;
+    }
+    return handler!(req, res);
+  } catch (err: any) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ ok: false, error: err?.message || String(err) }));
   }
-  return handler!(req, res);
 }
